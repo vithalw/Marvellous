f3e7920cba70df96623f4392d32652f8bcb9d39c
Vithal Waghmode <47021242+vithalw@users.noreply.github.com>
2019-11-04 12:34:12 +0530

    Add files via upload

---
+++ add1_3.py
@@ -0,0 +1,13 @@
+def add(no1,no2):
+    return no1+no2;
+
+
+def main():
+    no1=int(input("Enter 1st number: "));
+    no2=int(input("Enter 2nd number: "));
+    
+    print("Addition is: ",add(no1,no2))
+
+if __name__=="__main__":
+    main();
+    # TODO: write code...
